ca9c04e39d923f2d33916fda1c0ae6493c24b4d2
Kirito344 <85119549+Kirito344@users.noreply.github.com>
2023-02-11 04:55:08 -0800

    Update main.js

--- main.js
+++ main.js
@@ -8,13 +8,14 @@ let email = document.getElementsByClassName("email")[0]
 //var for  qr img
 let qrimg = document.getElementsByClassName("qr")[0]
 
-//var for qr generation
-let qrgen = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${email.value}`
+
+
 
 
 // when submitting registeration form
 form.onsubmit = function(e){
-
+ //var for qr generation from email
+ let qrgen = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${email.value}`
  qrimg.src = qrgen;
 e.preventDefault()
 }; 
