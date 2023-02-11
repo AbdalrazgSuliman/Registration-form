55a53f8b9c2dcbdd2ad586ea9469deb458eb7649
Kirito344 <85119549+Kirito344@users.noreply.github.com>
2023-02-11 04:06:40 -0800

    files for qr generation anf registration
    files for qr generation

---
+++ main.js
@@ -0,0 +1,21 @@
+//var for from
+let form =document.getElementsByClassName("form-style")[0]
+
+//var for get email text field
+let email = document.getElementsByClassName("email")[0]
+
+
+//var for  qr img
+let qrimg = document.getElementsByClassName("qr")[0]
+
+//var for qr generation
+let qrgen = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${email.value}`
+
+
+// when submitting registeration form
+form.onsubmit = function(e){
+
+ qrimg.src = qrgen;
+ qrimg.style.opacity = "1.0"
+e.preventDefault()
+}; 
